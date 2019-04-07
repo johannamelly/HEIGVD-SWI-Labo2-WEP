9c4b9b9361d71b58d82ba5d289b92bdf9632e86b
johannamelly <johannamelly@gmail.com>
2019-04-07 17:08:02 +0200

    part 2 done

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -7,51 +7,22 @@ import rc4
 
 #Cle wep AA:AA:AA:AA:AA
 key='\xaa\xaa\xaa\xaa\xaa'
-#lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
 arp = rdpcap('arp.cap')[0]
 
-data = arp.wepdata
-print(data)
-# rc4 seed est composé de IV+clé
-seed = arp.iv+key
-
-plain = "Knowing the source of any information is way more important than the information itself." 
+# setting plain text with 36 chars
 plain = "hello-world hello-world hello-world "
-##1. compute ICV of plain 
 
+##1. compute ICV of plain 
 icv = binascii.crc32(plain) 
-icv = struct.pack("!l", icv)
+icv = struct.pack("<i", icv)
 ##2. seed -> (RC4) = keystream
 ##3. keystream XOR (plain + ICV) = cipher
-cipher= rc4.re4encrypt(plain + icv, arp.iv, key)
+cipher= rc4.rc4crypt(plain + icv, arp.iv+key)
 
+##4. add fields
+arp.wepdata = cipher[:-4]
+(arp.icv,) = struct.unpack("!L", cipher[-4:])
 
-
-##4. add shit
-#arp.icv = long(icv.encode("hex"), 16)
-# arp.icv = 
-print(struct.unpack('!L', icv))
-arp.wepdata = cipher
-
-###5. Send shit
+###5. forge cap file
 wrpcap('arpForged.cap', arp)
 
-# recuperation de icv dans le message (arp.icv) (en chiffre) -- je passe au format "text". Il y a d'autres manières de faire ceci...
-icv_encrypted='{:x}'.format(arp.icv).decode("hex")
-
-# text chiffré y-compris l'icv
-message_encrypted=arp.wepdata+icv_encrypted
-
-# déchiffrement avec rc4
-#cleartext=rc4.rc4crypt(message_encrypted,seed)
-
-# le ICV est les derniers 4 octets - je le passe en format Long big endian
-#icv_enclair=cleartext[-4:]
-#(icv_numerique,)=struct.unpack('!L', icv_enclair)
-
-# le message sans le ICV
-#text_enclair=cleartext[:-4]
-
-#print 'Text: ' + text_enclair.encode("hex")
-#print 'icv:  ' + icv_enclair.encode("hex")
-#print 'icv(num): ' + str(icv_numerique)
